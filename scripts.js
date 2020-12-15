5c5f73f751cf1c4642a07c3e48485a2519f48391
Anders Dollerup <andersdollerup@MacBook-Pro-tilhrende-Anders.local>
2020-12-15 22:38:12 +0100

    d

--- scripts.js
+++ scripts.js
@@ -2,4 +2,13 @@ window.addEventListener("load", sidenVises);
 
 function sidenVises() {
     console.log("sidenVises");
+
+    document.querySelector("#menuknap").addEventListener("click", toggleMenu);
+
+
+}
+
+function toggleMenu() {
+    console.log("toggleMenu");
+    document.querySelector("#menu").classList.toggle("hidden");
 }
